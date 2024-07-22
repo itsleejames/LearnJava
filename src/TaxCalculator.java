b630f6065c67e04b9946030fccbc67c469813762
itsleejames <176151266+itsleejames@users.noreply.github.com>
2024-07-22 14:08:24 +0100

    2a ans

--- src/TaxCalculator.java
+++ src/TaxCalculator.java
@@ -1,5 +1,29 @@
 public class TaxCalculator {
+
     public static void main(String[] args) {
+
+
+        final int standardCutOff = 35_300;
+
+        final double lowerTaxRate = 0.2;
+
+        final double higherTaxRate = 0.4;
+        double tax = 0;
+
+
+        double income = 40_000; // imagine from keyboard
+
+
+        if (income > standardCutOff) {
+            tax = standardCutOff*lowerTaxRate;
+            double higherTaxableIncome = income - standardCutOff;
+            tax += higherTaxableIncome*higherTaxRate;
+ 
+        } else
+            tax = income * lowerTaxRate;
+
+        System.out.println(tax);
+
 
     }
 }
