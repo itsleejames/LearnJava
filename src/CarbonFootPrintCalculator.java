1b5a912527d4a9adc74b82e1b1a876d3de76e889
itsleejames <176151266+itsleejames@users.noreply.github.com>
2024-07-23 01:58:14 +0100

    q3 answear

--- src/CarbonFootPrintCalculator.java
+++ src/CarbonFootPrintCalculator.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+
 public class CarbonFootPrintCalculator {
+
+    public static void main(String[] args) {
+
+        String model_of_car = JOptionPane.showInputDialog("Enter Model of car");
+        String number_of_miles = JOptionPane.showInputDialog("Enter the number of miles per week");
+        String fuel_effi = JOptionPane.showInputDialog("Enter your cars fuel effiency");
+
+        double Miles = Double.parseDouble(number_of_miles);
+        double Fuel = Double.parseDouble(fuel_effi);
+
+        double C02Emissions = Miles*52 / Fuel;
+
+                System.out.println(C02Emissions);
+
+    }
 }
---
+++ src/rowsOfNums.java
@@ -0,0 +1,23 @@
+public class rowsOfNums {
+
+
+    public static void main(String[] args) {
+
+
+        for (int LineNum = 1; LineNum <= 5; LineNum++) {
+            for (int Number = 1; Number <= LineNum; Number++) {
+                System.out.print(Number + " ");
+            }
+            {
+                System.out.println();
+            }
+
+        }
+
+    }
+}
+
+
+
+
+
