e60afd050bde3ccb1135397535367fea59329670
itsleejames <176151266+itsleejames@users.noreply.github.com>
2024-07-24 14:19:59 +0100

    q3 ans

--- src/CarbonFootPrintCalculator.java
+++ src/CarbonFootPrintCalculator.java
@@ -2,18 +2,22 @@ import javax.swing.*;
 
 public class CarbonFootPrintCalculator {
 
+
     public static void main(String[] args) {
+        final int WEEKS_PER_YEAR = 52;
+        final double C02_PER_GALLON = 19.4;
+        final double OTHER_EMMISIONS = 100/95.0;
 
-        String model_of_car = JOptionPane.showInputDialog("Enter Model of car");
-        String number_of_miles = JOptionPane.showInputDialog("Enter the number of miles per week");
-        String fuel_effi = JOptionPane.showInputDialog("Enter your cars fuel effiency");
 
-        double Miles = Double.parseDouble(number_of_miles);
-        double Fuel = Double.parseDouble(fuel_effi);
+        String carModel = (JOptionPane.showInputDialog("Enter Model of car"));
+        double miles = Double.parseDouble(JOptionPane.showInputDialog("Enter the number of miles per week"));
+        double fuelEffiency = Double.parseDouble(JOptionPane.showInputDialog("Enter your cars fuel effiency"));
 
-        double C02Emissions = Miles*52 / Fuel;
 
-                System.out.println(C02Emissions);
+        double co2Emissions = (miles*WEEKS_PER_YEAR)/ fuelEffiency * C02_PER_GALLON * OTHER_EMMISIONS;
+
+        String output =  "%s Has a fuel effiecy of %.1f".formatted(carModel,co2Emissions);
+        JOptionPane.showMessageDialog(null, output, "CO2 EMISS", JOptionPane.INFORMATION_MESSAGE);
 
     }
 }
